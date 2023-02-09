60a0f77a42f9ddc91eed8638434cf6c56c1dd57a
francescod102 <Francescodesimone910@gmail.com>
2023-02-09 15:25:10 +0000

    Milestone 4 Task 2

---
+++ manual_rps.py
@@ -0,0 +1,39 @@
+from random import choice
+
+def get_computer_choice():
+    return choice(["Rock", "Paper", "Scissors"])
+
+def get_user_choice ():
+    user_choice = input("Enter Rock, Paper or Scissors: ")
+    return(user_choice)
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice  == 'Rock' and user_choice == 'Paper':
+       return "User won"
+    elif computer_choice == "Rock" and user_choice == "Scissors":
+       return "Computer won"
+    elif computer_choice == "Rock" and user_choice == "Rock":
+        return "it's a tie"
+    
+    if computer_choice  == 'Scissors' and user_choice == 'Rock':
+       return "User won"
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+       return "Computer won"
+    elif computer_choice == "Scissors" and user_choice == "Scissors":
+        return "it's a tie"
+    
+    if computer_choice  == 'Paper' and user_choice == 'Scissors':
+       return "User won"
+    elif computer_choice == "Paper" and user_choice == "Rock":
+       return "Computer won"
+    elif computer_choice == "Paper" and user_choice == "Paper":
+        return "it's a tie"
+    
+
+
+
+computer_choice = get_computer_choice()
+print(computer_choice)
+user_choice = get_user_choice()
+    
+print (get_winner(computer_choice,  user_choice))

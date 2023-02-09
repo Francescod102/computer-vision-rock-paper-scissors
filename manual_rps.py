99bda43a6873c066b05181f2e5095a268e76e913
francescod102 <Francescodesimone910@gmail.com>
2023-02-09 15:30:05 +0000

    fix words

--- manual_rps.py
+++ manual_rps.py
@@ -9,23 +9,23 @@ def get_user_choice ():
 
 def get_winner(computer_choice, user_choice):
     if computer_choice  == 'Rock' and user_choice == 'Paper':
-       return "User won"
+       return "You won"
     elif computer_choice == "Rock" and user_choice == "Scissors":
-       return "Computer won"
+       return "You lost"
     elif computer_choice == "Rock" and user_choice == "Rock":
         return "it's a tie"
     
     if computer_choice  == 'Scissors' and user_choice == 'Rock':
-       return "User won"
+       return "You won"
     elif computer_choice == "Scissors" and user_choice == "Paper":
-       return "Computer won"
+       return " You lost"
     elif computer_choice == "Scissors" and user_choice == "Scissors":
         return "it's a tie"
     
     if computer_choice  == 'Paper' and user_choice == 'Scissors':
-       return "User won"
+       return "You won"
     elif computer_choice == "Paper" and user_choice == "Rock":
-       return "Computer won"
+       return "You Lost"
     elif computer_choice == "Paper" and user_choice == "Paper":
         return "it's a tie"
     
@@ -33,7 +33,6 @@ def get_winner(computer_choice, user_choice):
 
 
 computer_choice = get_computer_choice()
-print(computer_choice)
 user_choice = get_user_choice()
     
 print (get_winner(computer_choice,  user_choice))

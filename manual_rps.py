058eebbf8d645f453c31a20f47dbad46424a205d
francescod102 <Francescodesimone910@gmail.com>
2023-02-13 16:53:28 +0000

    last change made

--- manual_rps.py
+++ manual_rps.py
@@ -10,34 +10,47 @@ def get_user_choice():
     return(user_choice)
 
 def get_winner(computer_choice, user_choice):
-    if computer_choice=='Rock':
-
-        if user_choice=='Rock':
-            print("it is a tie!")
-        elif user_choice=='Paper':
-            print('You won!')
-        else:
-            print("You lost")
-
-    elif  computer_choice=="Paper":
-
-        if user_choice=='Rock': 
-            print ("You lost")
-        elif user_choice=="Paper":
-            print ("it is a tie!") 
-        else:
-            print("You won!") 
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif computer_choice == 'Rock' and user_choice == 'Paper':
+        print('You won!')
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
+        print('You won!')
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
+        print('You won!')
+    else:
+        print('You lost')
 
 
-        
-    elif computer_choice=='Scissors':
+
+# def get_winner(computer_choice,user_choice):
+#     if computer_choice=='Rock':
+
+#         if user_choice=='Rock':
+#             print("it is a tie!")
+#         elif user_choice=='Paper':
+#             print('You won!')
+#         else:
+#             print("You lost")
+
+#     elif  computer_choice=="Paper":
+
+#         if user_choice=='Rock': 
+#             print ("You lost")
+#         elif user_choice=="Paper":
+#             print ("it is a tie!") 
+#         else:
+#             print("You won!") 
+
+
+#     elif computer_choice=='Scissors':
          
-        if user_choice=='Rock': 
-            print("You won!")
-        elif user_choice=="Paper": 
-            print("You lost")
-        else:
-            print ("it is a tie!")
+#         if user_choice=='Rock': 
+#             print("You won!")
+#         elif user_choice=="Paper": 
+#             print("You lost")
+#         else:
+#             print ("it is a tie!")
             
     
 print()

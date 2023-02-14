db511ad79161895e616452853a12f0f080817420
francescod102 <Francescodesimone910@gmail.com>
2023-02-14 20:50:37 +0000

    change made

--- manual_rps.py
+++ manual_rps.py
@@ -15,9 +15,9 @@ def get_winner(computer_choice,user_choice):
         print('It is a tie!')
     elif computer_choice=='Rock' and user_choice=='Paper':
         print('You won!')  
-    elif computer_choice=='Paper' and user_choice=='Scissor':
+    elif computer_choice=='Paper' and user_choice=='Scissors':
         print('You won!')
-    elif  computer_choice=="Scissor" and user_choice=='Rock':
+    elif  computer_choice=="Scissors" and user_choice=='Rock':
         print ('You won!') 
     else:
         print('You lost') 

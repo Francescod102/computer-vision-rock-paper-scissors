7deb784567d3aeff3e928f12f8ebc6f3f10ccc50
francescod102 <Francescodesimone910@gmail.com>
2023-02-14 21:46:40 +0000

    all done

--- camera_rps.py
+++ camera_rps.py
@@ -2,12 +2,12 @@
 # from keras.models import load_model
 # import numpy as np
 
-# def get_prediction():  
+# def get_prediction():
 #     model = load_model('keras_model.h5')
 #     cap = cv2.VideoCapture(0)
 #     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-#     while True: 
+#     while True:
 #         ret, frame = cap.read()
 #         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
 #         image_np = np.array(resized_frame)
@@ -19,7 +19,7 @@
 #         print(prediction)
 #         if cv2.waitKey(1) & 0xFF == ord('q'):
 #             break
-            
+
 #         # After the loop release the cap object
 #         cap.release()
 #         # Destroy all the windows
@@ -28,96 +28,95 @@
 # print (get_prediction())
 
 
-# import time
-# from datetime import datetime
+import time
+from datetime import datetime
 
-# script_start_time = datetime.now()
+script_start_time = datetime.now()
 
-# for i in range(100000):
-#     j = 10 + i
+for i in range(100000):
+    j = 10 + i
 
-# end_time = time.time()
-# final_time = end_time + script_start_time
-# print ( final_time)
+end_time = time.time()
+final_time = end_time + script_start_time
+print(final_time)
 
-# script_end_time = datetime.now()
-# print(script_end_time +script_start_time)
+script_end_time = datetime.now()
+print(script_end_time + script_start_time)
 
 
 import random
+
 
 def get_computer_choice():
     move_list = ["Rock", "Paper", "Scissors"]
     computer_choice = random.choice(move_list)
     return computer_choice
 
+
 def get_user_choice():
     user_choice = input("Enter Rock, Paper or Scissors: ")
     return user_choice
 
-computer_win = 0
-user_win  = 0
-       
-def get_winner(computer_choice,user_choice):
-    winner = None 
-    if computer_choice=='Rock':
-
-        if user_choice=='Rock':
-            print("it is a tie!")
-        
-        elif user_choice=='Paper':
-            print('You won!')
-            winner = "computer"
-            
-        else:
-            print("You lost")
-            winner = 'computer'
-            
-
-    elif  computer_choice=="Paper":
-
-        if user_choice=='Rock': 
-            print ("You lost")
-            winner = 'computer'
-            
-        elif user_choice=="Paper":
-            print ("it is a tie!") 
-            
-        else:
-            print("You won!") 
-            winner = 'user'
-            
-    elif computer_choice=='Scissors':
-        
-        if user_choice=='Rock': 
-            print("You won!")
-            winner ='user'
-            
-
-        elif user_choice=="Paper": 
-            print("You lost")
-            winner = 'computer'
-           
-        else:
-            print ("it is a tie!")
-              
-    return winner
 
 computer_win = 0
 user_win = 0
 
-while computer_win < 3 and user_win < 3 :
+
+def get_winner(computer_choice, user_choice):
+    winner = None
+    if computer_choice == "Rock":
+        if user_choice == "Rock":
+            print("it is a tie!")
+
+        elif user_choice == "Paper":
+            print("You won!")
+            winner = "computer"
+
+        else:
+            print("You lost")
+            winner = "computer"
+
+    elif computer_choice == "Paper":
+        if user_choice == "Rock":
+            print("You lost")
+            winner = "computer"
+
+        elif user_choice == "Paper":
+            print("it is a tie!")
+
+        else:
+            print("You won!")
+            winner = "user"
+
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You won!")
+            winner = "user"
+
+        elif user_choice == "Paper":
+            print("You lost")
+            winner = "computer"
+
+        else:
+            print("it is a tie!")
+
+    return winner
+
+
+computer_win = 0
+user_win = 0
+
+while computer_win < 3 and user_win < 3:
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
 
     winner = get_winner(computer_choice, user_choice)
-    if winner == 'computer': 
-        computer_win +=1
-    elif winner == "user" :
-        user_win +=1
-    print(computer_win,user_win)
-if computer_win > user_win :
+    if winner == "computer":
+        computer_win += 1
+    elif winner == "user":
+        user_win += 1
+    print(computer_win, user_win)
+if computer_win > user_win:
     print("computer_win")
-elif computer_win < user_win :
-    print('user_win')
-
+elif computer_win < user_win:
+    print("user_win")

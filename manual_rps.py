6db283a7e0ef21505f81552697bc2171e9ff691c
francescod102 <Francescodesimone910@gmail.com>
2023-02-14 18:20:31 +0000

    fixing

--- manual_rps.py
+++ manual_rps.py
@@ -9,71 +9,54 @@ def get_user_choice():
     user_choice = input("Enter Rock, Paper or Scissors: ")
     return user_choice
 
-computer_win = 0
-user_win  = 0
-       
+     
 def get_winner(computer_choice,user_choice):
-    winner = None 
     if computer_choice=='Rock':
 
         if user_choice=='Rock':
             print("it is a tie!")
-        
         elif user_choice=='Paper':
-            print('You won!')
-            winner = "computer"
-            
+            print('You won!')  
         else:
             print("You lost")
-            winner = 'computer'
-            
-
+             
     elif  computer_choice=="Paper":
 
         if user_choice=='Rock': 
             print ("You lost")
-            winner = 'computer'
-            
         elif user_choice=="Paper":
-            print ("it is a tie!") 
-            
+            print ("it is a tie!")  
         else:
             print("You won!") 
-            winner = 'user'
-            
+           
     elif computer_choice=='Scissors':
         
         if user_choice=='Rock': 
             print("You won!")
-            winner ='user'
-            
-
         elif user_choice=="Paper": 
             print("You lost")
-            winner = 'computer'
-           
         else:
             print ("it is a tie!")
               
-    return winner
+    # return winner
 
-computer_win = 0
-user_win = 0
+# computer_win = 0
+# user_win = 0
 
-while computer_win < 3 and user_win < 3 :
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
+# while computer_win < 3 and user_win < 3 :
+#     computer_choice = get_computer_choice()
+#     user_choice = get_user_choice()
 
-    winner = get_winner(computer_choice, user_choice)
-    if winner == 'computer': 
-        computer_win +=1
-    elif winner == "user" :
-        user_win +=1
-    print(computer_win,user_win)
-if computer_win > user_win :
-    print("computer_win")
-elif computer_win < user_win :
-    print('user_win')
+#     winner = get_winner(computer_choice, user_choice)
+#     if winner == 'computer': 
+#         computer_win +=1
+#     elif winner == "user" :
+#         user_win +=1
+#     print(computer_win,user_win)
+# if computer_win > user_win :
+#     print("computer_win")
+# elif computer_win < user_win :
+#     print('user_win')
 
 # user_continues = input ("Do you want to play ?( y/n)")
 # if user_choice in ['Yes']:
@@ -82,14 +65,14 @@ elif computer_win < user_win :
 #     else:
 #     break
 
-    # computer_choice = get_computer_choice()
-    # print (computer_choice)
-    # user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    print (computer_choice)
+    user_choice = get_user_choice()
 
-    # print (get_winner (computer_choice ,user_choice,))
+    print (get_winner (computer_choice ,user_choice,))
     
-# def play ():
-#         play = (get_computer_choice,get_user_choice,get_winner)
+def play ():
+        play = (get_computer_choice,get_user_choice,get_winner)
         
                         
                         
